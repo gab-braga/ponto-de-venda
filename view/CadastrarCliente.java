cd6f899cef9c69a5b0c5914b38b4a9040a5a290e
F-Gabriel-Braga <braagaa.inf@hotmail.com>
2021-07-08 22:47:49 -0300

    ADD VIEW

---
+++ view/CadastrarCliente.java
@@ -0,0 +1,47 @@
+package view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+public class CadastrarCliente extends Application {
+    private final String title = "Cadastrar Cliente";
+
+    private static Stage window;
+
+    private void setWindow(Stage window) {
+        this.window = window;
+    }
+
+    public static Stage getWindow() {
+        return window;
+    }
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("fxml/cadastrar_cliente.fxml"));
+            Scene scene = new Scene(root);
+
+            setWindow(stage);
+            stage.setScene(scene);
+            stage.initStyle(StageStyle.DECORATED);
+            stage.setTitle(title);
+            stage.setResizable(false);
+            stage.show();
+        }
+        catch (IOException ex) {
+            System.err.println(String.format("ERRO (%s): ", title));
+            ex.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ view/Login.java
@@ -0,0 +1,26 @@
+package view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Login extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(Login.class.getResource("fxml/login.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("Login");
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
